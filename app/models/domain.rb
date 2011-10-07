21b76708490ba563b3380de5810037b6bec587bb
Jinze Li <jinze_li@yahoo.com>
2011-10-08 01:16:40 +0200

    Finished upload feature

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,8 +1,10 @@
 class Domain < ActiveRecord::Base
-  attr_accessible :title, :description, :pdomain_delete, :pdomain
+  attr_accessible :title, :description, :pdomain_delete, :pdomain, :pproblem_delete, :pproblem
   has_attached_file :pdomain
+  has_attached_file :pproblem
   
   before_save :destroy_pdomain?
+  before_save :destroy_pproblem?
 
   def pdomain_delete
     @pdomain_delete ||= "0"
@@ -11,10 +13,20 @@ class Domain < ActiveRecord::Base
   def pdomain_delete=(value)
     @pdomain_delete = value
   end
+  
+  def pproblem_delete
+    @pproblem_delete ||= "0"
+  end
 
+  def pproblem_delete=(value)
+    @pproblem_delete = value
+  end
 private
   def destroy_pdomain?
     self.pdomain.clear if @pdomain_delete == "1"
+  end
+  def destroy_pproblem?
+    self.pproblem.clear if @pproblem_delete == "1"
   end
 
 end
